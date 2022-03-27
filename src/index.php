b61917cf11a0da67c204bb94dba08d8419de94d5
Dev Renesanco <dev@renesanco.com>
2022-03-27 20:05:04 +0900

    WP Theme: first commit

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+bloginfo();
+while(have_posts()) {
+    the_post();
+    ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <p><?php the_content(); ?></p>
+    <hr>
+    <?php
+}
+?>
